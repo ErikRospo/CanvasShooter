247ec885fe137cf7ceae965f831a8aa1b58aac42
Erik Rospo <rospoerik@gmail.com>
2022-03-15 10:15:32 -0700

    Removed excess files that had been added by a pull request

--- src/ts/helperFunctions.ts
+++
@@ -1,7 +0,0 @@
-function logx(val: number, base: number) {
-    return Math.log(val) / Math.log(base);
-}
-
-function randomBetween(min: number, max: number) {
-    return Math.random() * (max - min) + min
-}
