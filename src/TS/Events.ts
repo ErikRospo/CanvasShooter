f0034c3c5559d42f14cba03d45aff4ad2f47c6a0
ErikRospo <87732519+ErikRospo@users.noreply.github.com>
2022-06-03 12:17:35 -0700

    Removed some excess parts of init.ts, and Events.ts

--- src/TS/Events.ts
+++ src/TS/Events.ts
@@ -47,7 +47,6 @@ OptionsAimSlider.addEventListener("change", () => {
     } else {
         ShowPlayerAim = true;
     }
-    // player.barrelRadius = Number(OptionsAimSlider.value);
     player.spread = Number(OptionsAimSlider.value);
 
 });
@@ -70,45 +69,3 @@ OptionsMusicSlider.addEventListener("change", () => {
     MusicPlayer.shuffle();
     MusicPlayer.continue = true;
 });
-// MainMenuMuteButton.addEventListener("onclick", () => {
-//     console.log("Mute Button Clicked!");
-//     updateMuteBTN(!SFXMuted);
-// })
-// MainMenuOptionsButton.addEventListener("onclick", () => {
-//     OpenOptionsMenu();
-//     OptionsOpen = true;
-// })
-// MainMenuStartButton.addEventListener("onclick", () => {
-//     MainMenu.style.display = "none";
-//     ModalEL.style.display = "none";
-// });
-// MainMenuStartButton.onclick = () => {
-//     ModalEL.style.display = "none";
-//     MainMenu.style.display = "none";
-//     GameStarted = true;
-//     init();
-//     animate();
-
-// }
-// //add the event listener for the start button
-// MainMenuStartButton.addEventListener("onclick", () => {
-//     console.log("Start Button Clicked!");
-//     StartGame()
-// });
-// // MainMenuStartButton.attributes.item
-// startGameButton.addEventListener("click", () => {
-//     ModalEL.style.display = "none"
-//     init();
-//     animate();
-//     //hide the UI
-// });
-// function StartGame() {
-//     console.log("Started Game!");
-
-//     MainMenu.style.display = "none";
-//     ModalEL.style.display = "none";
-//     GameStarted = true;
-//     init();
-//     animate();
-// }
-// console.log(MainMenuStartButton)
--- src/TS/init.ts
+++ src/TS/init.ts
@@ -22,35 +22,22 @@ c.shadowColor = "black";
 const scoreEL = document.querySelector("#scoreEL") as HTMLSpanElement;
 const MoneyEL = document.querySelector("#moneyEL") as HTMLSpanElement;
 const ShopMoney = document.querySelector("#ShopMoney") as HTMLSpanElement;
-const startGameButton = document.querySelector(
-    "#StartGameWrapper"
-) as HTMLDivElement;
-const HighScoreLabel = document.querySelector(
-    "#highScoreTextModal"
-) as HTMLParagraphElement;
+const startGameButton = document.querySelector("#StartGameWrapper") as HTMLDivElement;
+const HighScoreLabel = document.querySelector("#highScoreTextModal") as HTMLParagraphElement;
 const ModalEL = document.querySelector("#ModalEL") as HTMLDivElement;
 const TitleEL = document.querySelector("#titleElement") as HTMLHeadingElement;
 const BigScoreEL = document.querySelector("#BigScoreEL") as HTMLHeadingElement;
-const BigScoreELLabel = document.querySelector(
-    "#PointsLabelEL"
-) as HTMLSpanElement;
+const BigScoreELLabel = document.querySelector("#PointsLabelEL") as HTMLSpanElement;
 const NameDiv = document.querySelector("#NameInputDiv") as HTMLDivElement;
 const HighScoreList = document.querySelector("#HighScore") as HTMLOListElement;
 HighScoreList.style.display = "block";
 //Sound Effects
 let relPath = PROD ? "/CanvasShooter/" : "";
 const ShootSound = new Audio(relPath + "Audio/sound/Shoot.wav") as HTMLAudioElement;
-const HitNoKillSound = new Audio(
-    relPath + "Audio/sound/HitNoKill.wav"
-) as HTMLAudioElement;
-const HitAndKillSound = new Audio(
-    relPath + "Audio/sound/HitAndKill.wav"
-) as HTMLAudioElement;
-const HealthGetSound = new Audio(
-    relPath + "Audio/sound/HealthGet.wav"
-) as HTMLAudioElement;
-const HealthLoseSound = new Audio(relPath + "Audio/sound/HealthLose.wav"
-) as HTMLAudioElement;
+const HitNoKillSound = new Audio(relPath + "Audio/sound/HitNoKill.wav") as HTMLAudioElement;
+const HitAndKillSound = new Audio(relPath + "Audio/sound/HitAndKill.wav") as HTMLAudioElement;
+const HealthGetSound = new Audio(relPath + "Audio/sound/HealthGet.wav") as HTMLAudioElement;
+const HealthLoseSound = new Audio(relPath + "Audio/sound/HealthLose.wav") as HTMLAudioElement;
 const MissSound = new Audio(relPath + "Audio/sound/Miss.wav") as HTMLAudioElement;
 // define music
 const Music1 = new Audio(relPath + "Audio/music/Music1.mp3") as HTMLAudioElement;
@@ -62,36 +49,18 @@ const Music5 = new Audio("Audio/music/Music5.mp3") as HTMLAudioElement;
 
 //Pause Menu
 const PauseModal = document.querySelector("#PauseModal") as HTMLDivElement;
-const PauseModalScore = document.querySelector(
-    "#PauseModalScore"
-) as HTMLSpanElement;
-const PauseModalScoreLabel = document.querySelector(
-    "#PauseModalScoreLabel"
-) as HTMLSpanElement;
-const PauseModalOptionsButton = document.querySelector(
-    "#PauseModalOptionsButton"
-) as HTMLButtonElement;
-const PauseModalPlayButton = document.querySelector(
-    "#PauseModalPlayButton"
-) as HTMLButtonElement;
+const PauseModalScore = document.querySelector("#PauseModalScore") as HTMLSpanElement;
+const PauseModalScoreLabel = document.querySelector("#PauseModalScoreLabel") as HTMLSpanElement;
+const PauseModalOptionsButton = document.querySelector("#PauseModalOptionsButton") as HTMLButtonElement;
+const PauseModalPlayButton = document.querySelector("#PauseModalPlayButton") as HTMLButtonElement;
 
 //Options Menu
 const OptionsMenu = document.querySelector("#OptionsModal") as HTMLDivElement;
-const OptionsSFXSlider = document.querySelector(
-    "#SFXSlider"
-) as HTMLInputElement;
-const OptionsMusicSlider = document.querySelector(
-    "#MusicSlider"
-) as HTMLInputElement;
-const OptionsParticleSwitch = document.querySelector(
-    "#ParticleOptionsSwitch"
-) as HTMLInputElement;
-const OptionsBackButton = document.querySelector(
-    "#OptionsBackButton"
-) as HTMLButtonElement;
-const OptionsParticleSpan = document.querySelector(
-    "#ParticleOptionsSpan"
-) as HTMLSpanElement;
+const OptionsSFXSlider = document.querySelector("#SFXSlider") as HTMLInputElement;
+const OptionsMusicSlider = document.querySelector("#MusicSlider") as HTMLInputElement;
+const OptionsParticleSwitch = document.querySelector("#ParticleOptionsSwitch") as HTMLInputElement;
+const OptionsBackButton = document.querySelector("#OptionsBackButton") as HTMLButtonElement;
+const OptionsParticleSpan = document.querySelector("#ParticleOptionsSpan") as HTMLSpanElement;
 const OptionsAimSlider = document.querySelector("#OptionsAimSlider") as HTMLInputElement;
 if (DEBUGFLAG) {
     console.log(OptionsAimSlider);
@@ -99,13 +68,6 @@ if (DEBUGFLAG) {
 //debugging elements:
 const debugDiv = document.querySelector("#debugDiv") as HTMLDivElement;
 const debugList = document.querySelector("#debugList") as HTMLUListElement;
-//Main Menu
-// const MainMenu = document.querySelector("#MainMenu") as HTMLDivElement;
-// const MainMenuGameTitle = document.querySelector("#MainMenuGameTitle") as HTMLHeadingElement;
-// const MainMenuStartButton = document.querySelector("#MainMenuStartButton") as HTMLButtonElement;
-// const MainMenuMuteButton = document.querySelector("#MainMenuMuteButton") as HTMLButtonElement;
-// console.log(MainMenuStartButton.innerHTML);
-// const MainMenuOptionsButton = document.querySelector("#MainMenuOptionsButton") as HTMLButtonElement;
 //define other global constants
 const w = canvas.width;
 const h = canvas.height;
